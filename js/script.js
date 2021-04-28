3a5d15987bd1f1e4e94332c3d75d8c999e5bb3b6
石川大地 <ishikawadaichi@daichinoMacBook-Air.local>
2021-04-28 18:43:34 +0900

    [Finish]SlideShow_Lesson

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.theTarget').skippr({
+  transiton: 'fade',
+  speed: 1000,
+  easing: 'easeOutQuart',
+  navType: 'block',
+  childrenElementType: 'div',
+  arrows: true,
+  autoPlay: true,
+  autoPlayDuration: 1000,
+  KeyboardOnAlways: true,
+  hidePrevious: false
+})
